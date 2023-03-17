2900b0f93e4ca305c693ee1132ca3273d2a27d0c
Jaramill0 <hugo98jaramillo@gmail.com>
2023-03-16 20:38:42 -0600

    CRUD Cliente

---
+++ crud/resources/views/cliente/form.blade.php
@@ -0,0 +1,19 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('nombre') }}
+            {{ Form::text('nombre', $cliente->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('razon_social') }}
+            {{ Form::text('razon_social', $cliente->razon_social, ['class' => 'form-control' . ($errors->has('razon_social') ? ' is-invalid' : ''), 'placeholder' => 'Razon Social']) }}
+            {!! $errors->first('razon_social', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</div>
---
+++ crud/resources/views/cliente/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('template_title')
+    Cliente
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+
+                            <span id="card_title">
+                                {{ __('Cliente') }}
+                            </span>
+
+                             <div class="float-right">
+                                <a href="{{ route('clientes.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
+                                  {{ __('Crear Nuevo Usuario') }}
+                                </a>
+                                <a href="{{ route('home') }}" class="btn btn-info btn-sm float-right"  data-placement="left">
+                                    {{ __('Regresar a Inicio') }}
+                                  </a>
+                              </div>
+                        </div>
+                    </div>
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                    @endif
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <thead class="thead">
+                                    <tr>
+                                        <th>No</th>
+                                        
+										<th>Nombre</th>
+										<th>Razon Social</th>
+
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($clientes as $cliente)
+                                        <tr>
+                                            <td>{{ ++$i }}</td>
+                                            
+											<td>{{ $cliente->nombre }}</td>
+											<td>{{ $cliente->razon_social }}</td>
+
+                                            <td>
+                                                <form action="{{ route('clientes.destroy',$cliente->id) }}" method="POST">
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('clientes.show',$cliente->id) }}"><i class="fa fa-fw fa-eye"></i>Ver</a>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('clientes.edit',$cliente->id) }}"><i class="fa fa-fw fa-edit"></i>Editar</a>
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i>Eliminar</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                {!! $clientes->links() !!}
+            </div>
+        </div>
+    </div>
+@endsection
